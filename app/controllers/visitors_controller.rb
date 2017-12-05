f119f649add954dd9aea3f3419342cb5ab443660
winebarrel <sugawara@winebarrel.jp>
2017-12-05 16:21:05 +0900

    beautify search query

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,11 @@
 class VisitorsController < ApplicationController
   def index
     @search = params[:search]
+
+    if @search
+      @search = beautify_json(@search)
+    end
+
     @indices = Index.all
     @queries = Query.order(:name)
 
@@ -28,5 +33,14 @@ class VisitorsController < ApplicationController
 
   def query
     render plain: Query.find(params[:query]).query
+  end
+
+  private
+
+  def beautify_json(json)
+    hash = JSON.parse(json)
+    JSON.pretty_generate(hash)
+  rescue JSON::ParserError
+    json
   end
 end
