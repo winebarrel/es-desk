5c66898e159f047ef22b4189e0e7671a1d01d0b0
winebarrel <sugawara@winebarrel.jp>
2017-12-22 06:32:18 +0900

    Fix resultsets#index

--- app/controllers/resultsets_controller.rb
+++ app/controllers/resultsets_controller.rb
@@ -3,7 +3,7 @@ class ResultsetsController < ApplicationController
 
   # GET /resultsets
   def index
-    @resultsets = Resultset.order(created_at: :desc).page(params[:page])
+    @resultsets = Resultset.select_without_data.order(created_at: :desc).page(params[:page])
   end
 
   # GET /resultsets/:id
